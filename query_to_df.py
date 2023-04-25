ab8b416784bcce6ba91c477bdee69e636007b093
Ivan Suarez <ivan.suarez.fabara@gmail.com>
2023-04-25 15:09:53 -0500

    Query to DB working correctly

--- query_to_df.py
+++ query_to_df.py
@@ -1,15 +1,30 @@
+import os
 import pandas as pd
 import sqlalchemy as sa
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 
+"""Select download route"""
+download_route = 'C:/Users/isuarez/Downloads'
+
+"""SERVER Information"""
 DRIVER_NAME = 'ODBC Driver 11 for SQL Server'
 SERVER_NAME = '172.20.1.220\SQL2014'
-DATABASE_NAME = '8_CZO2'
-TABLE = 'dbo.Script_ResultMetric_TEMS_Service_KPIs_HTTP'
+DATABASE_NAME = 'AnalyticsMeasDB'
+TABLE1 = 'cdr.SessionSummary'
+TABLE2 = 'cdr.SessionSummaryData'
+
+start_date = '2022-07-18 00:00:00.001'
+end_date = '2022-07-22 23:59:59.999'
 
 # Define the SQL query
-sql_query = f'SELECT TOP 100 * FROM {TABLE};'
+sql_query1 = f"SELECT DatasourceId, SessionIdOrCallIndex, SessionType, StartTime, StartLatitude, StartLongitude, " \
+             f"StartRadioTechnology, EndTime, EndLatitude, EndLongitude, EndRadioTechnology, SimOperator, IMSI, " \
+             f"IMEI, SessionEndStatus FROM {TABLE1} WHERE StartTime BETWEEN '{start_date}' AND '{end_date}';"
+sql_query2 = f"SELECT DatasourceId, SessionId, SessionType, StartDateTime, EndDateTime, EndServiceBearer, " \
+             f"EndDataRadioBearer, EndFileSize, EndServiceStatus, IPServiceSetupTimeMethodAMethod, " \
+             f"DataTransferTimeMethodADuration FROM {TABLE2} WHERE StartDateTime >= '{start_date}' " \
+             f"AND StartDateTime <= '{end_date}';"
 
 try:
     # Create the engine according to SQLAlchemy documentation
@@ -19,13 +34,19 @@ try:
 
     # Establish a connection to the database, execute the query and create the df with the results
     with engine.begin() as conn:
-        df = pd.read_sql_query(sa.text(sql_query), conn)
+        df1 = pd.read_sql_query(sa.text(sql_query1), conn)
+        df2 = pd.read_sql_query(sa.text(sql_query2), conn)
 
     # Close the connection object
     conn.close()
 
-    # Print df to see the content of df
-    print(df)
-
 except Exception as e:
     print('Database connection failed.')
+
+# Pass the df to a .csv file
+df1.to_csv('data1.csv', index=False, sep=';', encoding='utf-8', header=True)
+df2.to_csv('data2.csv', index=False, sep=';', encoding='utf-8', header=True)
+
+# Download the .csv file
+os.rename('data1.csv', f'{download_route}/data1.csv')
+os.rename('data2.csv', f'{download_route}/data2.csv')
