1706dfdb26bc62abd709d83c1b8c96ee6b8b2f72
Ivan Suarez <ivan.suarez.fabara@gmail.com>
2023-04-27 12:06:09 -0500

    Added Operator Option Menu and Throughput calculation

--- query_to_df.py
+++ query_to_df.py
@@ -3,8 +3,8 @@ import tkinter as tk
 import tkcalendar as tc
 from tkinter import messagebox
 import pandas as pd
-import datetime
 import sqlalchemy as sa
+import datetime
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 
@@ -37,7 +37,8 @@ class SammApp(tk.Frame):
         self.master = master
         self.master.title("Reportes SAMM")
         self.master.geometry("800x450")
-
+        self.list_of_operators = ["Claro", "CNT", "Movistar"]
+        self.operadora = tk.StringVar()
         self.RepGen = tk.BooleanVar()
         self.create_widgets()
         self.program_is_running = False
@@ -45,17 +46,37 @@ class SammApp(tk.Frame):
     def create_widgets(self):
         """Create the widges to be used with tkinter and tkcalendar"""
 
-        self.lbl_3 = tk.Label(self.master, text="Fecha inicio:", width=10, font=("bold", 11))
-        self.lbl_3.grid(row=0, column=0, sticky=tk.W, padx=250)
+        self.lbl_1 = tk.Label(self.master, text="Operadora:", width=20, font=("bold", 11))
+        self.lbl_1.grid(row=0, column=0, sticky=tk.W)
+
+        self.option_menu1 = tk.OptionMenu(self.master, self.operadora, *self.list_of_operators)
+        self.option_menu1.grid(row=0, column=0, sticky=tk.W, padx=130)
+
+        self.lbl_2 = tk.Label(self.master, text="Fecha inicio:", width=10, font=("bold", 11))
+        self.lbl_2.grid(row=0, column=0, sticky=tk.W, padx=250)
 
         self.fecha_inicio = tc.DateEntry(self.master, selectmode='day', date_pattern='yyyy-mm-dd')
         self.fecha_inicio.grid(row=0, column=0, sticky=tk.W, padx=355)
 
-        self.lbl_4 = tk.Label(self.master, text="Fecha fin:", width=10, font=("bold", 11))
-        self.lbl_4.grid(row=0, column=0, sticky=tk.W, padx=450)
+        self.lbl_3 = tk.Label(self.master, text="Fecha fin:", width=10, font=("bold", 11))
+        self.lbl_3.grid(row=0, column=0, sticky=tk.W, padx=450)
 
         self.fecha_fin = tc.DateEntry(self.master, selectmode='day', date_pattern='yyyy-mm-dd')
         self.fecha_fin.grid(row=0, column=0, sticky=tk.W, padx=545)
+
+        self.lbl_4 = tk.Label(self.master, text="Hora inicio:", width=10, font=("bold", 11))
+        self.lbl_4.grid(row=1, column=0, sticky=tk.W, padx=250, pady=10)
+
+        self.tiempo_inicio = tk.Entry(self.master)
+        self.tiempo_inicio.insert(0, "HH:mm")
+        self.tiempo_inicio.grid(row=1, column=0, sticky=tk.W, padx=355, pady=10)
+
+        self.lbl_5 = tk.Label(self.master, text="Hora fin:", width=10, font=("bold", 11))
+        self.lbl_5.grid(row=1, column=0, sticky=tk.W, padx=450, pady=10)
+
+        self.tiempo_fin = tk.Entry(self.master)
+        self.tiempo_fin.insert(0, "HH:mm")
+        self.tiempo_fin.grid(row=1, column=0, sticky=tk.W, padx=545, pady=10)
 
     def start(self):
         """Define start button actions"""
@@ -76,31 +97,46 @@ class SammApp(tk.Frame):
 
     def program(self):
         """Define base program actions. All data analysis is presented in this section of the code."""
+        operadora = self.operadora.get()
         fecha_inicio = self.fecha_inicio.get_date().strftime("%Y-%m-%d")
         fecha_fin = self.fecha_fin.get_date().strftime("%Y-%m-%d")
+        tiempo_inicio = self.tiempo_inicio.get()
+        tiempo_fin = self.tiempo_fin.get()
 
         # Add HH:MM:SS to fecha_inicio and fecha_fin so the range of dates we want to show in the report is correct
-        # (reminder: the dates we enter in the form is just a string with format 2022-01-12, that is why we made this
-        # change to not lose information when we present the data at the end)
-        add_string1 = ' 00:00:01'
-        add_string2 = ' 23:59:59'
+        add_string1 = f' {tiempo_inicio}:00'
+        add_string2 = f' {tiempo_fin}:00'
         fecha_inicio += add_string1
         fecha_fin += add_string2
 
+        # Definitions:
         def convert(date_time):
             """function to convert a string to datetime object"""
-            format = '%Y-%m-%d %H:%M:%S'  # The format
-            datetime_str = datetime.datetime.strptime(date_time, format)
+            date_format = '%Y-%m-%d %H:%M:%S'  # The format
+            datetime_str = datetime.datetime.strptime(date_time, date_format)
             return datetime_str
+
+        def throughput(row):
+            """function to return the throughput value """
+            if row['EndFileSize'] != 0 and row['EndServiceStatus'] == 'Succeeded' and isinstance(
+                    row['DataTransferTimeMethodADuration'], datetime.time):
+                total_seconds = (row['DataTransferTimeMethodADuration'].hour * 3600) + (
+                        row['DataTransferTimeMethodADuration'].minute * 60) + row[
+                                    'DataTransferTimeMethodADuration'].second + row[
+                                    'DataTransferTimeMethodADuration'].microsecond / 1000000
+                return ((float(row['EndFileSize']) * 8) / total_seconds) / 1000
+            else:
+                return None
 
         # Convert fecha_inicio and fecha_fin to datetime object
         fecha_inicio = convert(fecha_inicio)
         fecha_fin = convert(fecha_fin)
 
         # Define the SQL query
-        sql_query1 = f"SELECT DatasourceId, SessionIdOrCallIndex, SessionType, StartTime, StartLatitude, StartLongitude, " \
-                     f"StartRadioTechnology, EndTime, EndLatitude, EndLongitude, EndRadioTechnology, SimOperator, IMSI, " \
-                     f"IMEI, SessionEndStatus FROM {TABLE1} WHERE StartTime BETWEEN '{fecha_inicio}' AND '{fecha_fin}';"
+        sql_query1 = f"SELECT DatasourceId, SessionIdOrCallIndex, SessionType, StartTime, StartLatitude, " \
+                     f"StartLongitude, StartRadioTechnology, EndTime, EndLatitude, EndLongitude, EndRadioTechnology, " \
+                     f"SimOperator, IMSI, IMEI, SessionEndStatus FROM {TABLE1} " \
+                     f"WHERE StartTime BETWEEN '{fecha_inicio}' AND '{fecha_fin}';"
         sql_query2 = f"SELECT DatasourceId, SessionId, SessionType, StartDateTime, EndDateTime, EndServiceBearer, " \
                      f"EndDataRadioBearer, EndFileSize, EndServiceStatus, IPServiceSetupTimeMethodAMethod, " \
                      f"DataTransferTimeMethodADuration FROM {TABLE2} WHERE StartDateTime >= '{fecha_inicio}' " \
@@ -108,14 +144,18 @@ class SammApp(tk.Frame):
 
         try:
             # Create the engine according to SQLAlchemy documentation
-            connection_string = f"DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME};Trusted_Connection=yes;"
+            connection_string = f"DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME};" \
+                                f"Trusted_Connection=yes;"
             connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
             engine = create_engine(connection_url)
 
+            # define decimal separator as comma
+            decimal_sep = ","
+
             # Establish a connection to the database, execute the query and create the df with the results
             with engine.begin() as conn:
-                df1 = pd.read_sql_query(sa.text(sql_query1), conn)
-                df2 = pd.read_sql_query(sa.text(sql_query2), conn)
+                df1 = pd.read_sql_query(sa.text(sql_query1), conn, params={"decimal": decimal_sep})
+                df2 = pd.read_sql_query(sa.text(sql_query2), conn, params={"decimal": decimal_sep})
 
             # Close the connection object
             conn.close()
@@ -123,20 +163,36 @@ class SammApp(tk.Frame):
         except Exception as e:
             print('Database connection failed.')
 
-        # Pass the df to a .csv file
-        df1.to_csv('SessionSummary.csv', index=False, sep=';', encoding='utf-8', header=True)
-        df2.to_csv('SessionSummaryData.csv', index=False, sep=';', encoding='utf-8', header=True)
+        # Rename columns in the dataframes
+        df1 = df1.rename(columns={'SessionIdOrCallIndex': 'SessionId', 'SessionEndStatus': 'EndServiceStatus'})
+        df2 = df2.rename(columns={'StartDateTime': 'StartTime', 'EndDateTime': 'EndTime'})
+
+        # Merge df1 and df2 by: 'DatasourceId', 'SessionId', 'SessionType', 'StartTime', 'EndTime', 'EndServiceStatus'
+        df_final = df1.merge(df2, how='right', on=['DatasourceId', 'SessionId', 'SessionType', 'StartTime', 'EndTime',
+                                                   'EndServiceStatus'])
+
+        df_final['ThroughputKbps'] = df_final.apply(lambda row: throughput(row), axis=1)
+
+        if operadora == 'Claro':
+            df_final = df_final.loc[df_final['SimOperator'] == 'Claro']
+            df_final = df_final.loc[df_final['EndServiceStatus'] == 'Succeeded']
+        elif operadora == 'CNT':
+            df_final = df_final.loc[df_final['SimOperator'] == 'CNT']
+            df_final = df_final.loc[df_final['EndServiceStatus'] == 'Succeeded']
+        elif operadora == 'Movistar':
+            df_final = df_final.loc[df_final['SimOperator'] == 'Movistar']
+            df_final = df_final.loc[df_final['EndServiceStatus'] == 'Succeeded']
+
+        # Pass the df_final to a .csv file
+        df_final.to_csv(f'SessionSummary{operadora}.csv', index=False, sep=';', encoding='utf-8', header=True,
+                        decimal=',')
 
         # Remove the previous files if already exist
-        if os.path.exists(f'{download_route}/SessionSummary.csv'):
-            os.remove(f'{download_route}/SessionSummary.csv')
-
-        if os.path.exists(f'{download_route}/SessionSummaryData.csv'):
-            os.remove(f'{download_route}/SessionSummaryData.csv')
+        if os.path.exists(f'{download_route}/SessionSummary{operadora}.csv'):
+            os.remove(f'{download_route}/SessionSummary{operadora}.csv')
 
         # Download the .csv file
-        os.rename('SessionSummary.csv', f'{download_route}/SessionSummary.csv')
-        os.rename('SessionSummaryData.csv', f'{download_route}/SessionSummaryData.csv')
+        os.rename(f'SessionSummary{operadora}.csv', f'{download_route}/SessionSummary{operadora}.csv')
 
 
 if __name__ == "__main__":
